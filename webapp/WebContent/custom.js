a379fbba290ceece7ad5d86a2e9382ea961de105
admin <admin@PCSS-AP-W8>
2015-02-11 17:27:50 +0100

    generyczne poi

--- webapp/WebContent/custom.js
+++ webapp/WebContent/custom.js
@@ -1,16 +1,50 @@
 var Solr = {
-	getData: function () {
+	getData : function() {
+		var that = this;
+
 		$.ajax({
-			src: "solr.json",
-			success: function (data) {
-				console.log(data);
+			url : "solr.json",
+			success : function(data) {
+				var jsonData = $.parseJSON(data);
+
+				that.setPlaces(jsonData.response.docs);
 			}
 		});
-	},	
-		
+	},
+	setPlaces : function(places) {
+		for ( var element in places) {
+
+			var meta = places[element];
+
+			console.log(meta);
+
+			var template = $($('.templates .place').html());
+			$(template).find('input[type="text"]').val(meta.title);
+
+			$(template).find('input[type="text"]').attr('data-title',
+					meta.title);
+			$(template).find('input[type="text"]').attr('data-description',
+					meta.description);
+			$(template).find('input[type="text"]').attr('data-picture',
+					meta.picture);
+			$(template).find('input[type="text"]').attr('data-location',
+					meta.location);
+
+			$('.sidebar .nav').append(template);
+
+		}
+	}
+
 }
 
-$(function () {
+$(function() {
 	Solr.getData();
-	
+
+	$(".sidebar").delegate(".point-checkbox", "click", function() {
+
+
+		
+		calcRoute();
+	});
+
 })
